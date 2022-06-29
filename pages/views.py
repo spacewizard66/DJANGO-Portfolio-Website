275678cc08d06aa7d3bab314861a2505a343499d
Mason Galat <70931763+spacewizard66@users.noreply.github.com>
2022-06-29 02:47:29 -0400

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -7,14 +7,8 @@ from projects.models import Project
 def home_view(request, *args, **kwargs):
     #print(request.user)
     #return HttpResponse("<h1>Django Home Page</h1>") # string of HTML
-    return render(request, 'homepage.html')
-
-def what_view2(request, *args, **kwargs):
-    return HttpResponse("")
-
-def project_index(request):
     projects = Project.objects.all()
     context = {
         'projects': projects
     }
-    return render(request, 'portfoliopage.html', context)
+    return render(request, 'homepage.html', context)
